d689762b5ba84b7a4a36d0fd58dde4bfe73776fe
Lê Việt Thắng <levietthang1997@gmail.com>
2021-04-28 14:24:10 +0700

    Initial commit

---
+++ src/main/java/com/adt/adtwebtemplate/AdtWebTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.adt.adtwebtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdtWebTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdtWebTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adt/adtwebtemplate/AdtWebTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adt.adtwebtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdtWebTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
